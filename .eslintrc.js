bfe4afe37e8428af7e8fb187b10aa71d676a9c3f
kagerka <kagerka@gmail.com>
2023-03-07 12:55:44 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+    'no-multi-spaces': ['error'],
+  },
+  plugins: ['eslint-plugin-html'],
+};
